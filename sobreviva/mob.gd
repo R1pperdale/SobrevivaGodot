0734ee993835420bddbc4efe145860d9898da60b
Bruno <brunoranulfi@gmail.com>
2025-06-10 10:35:46 -0300

    Add files via upload

---
+++ sobreviva/mob.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+
+func _ready():
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
+
+
+func _process(delta):
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+		queue_free()
+		
